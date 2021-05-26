6aafd7540a5c3972ed046fd44b584f177993da81
Yogesh Bhanushali <bhanushali.yo@gmail.com>
2021-05-26 22:18:58 +0530

    Partially Search Complete

--- app/src/main/java/com/example/casestudy/searchFragment.java
+++ app/src/main/java/com/example/casestudy/searchFragment.java
@@ -18,9 +18,12 @@ import android.app.SearchManager;
 import android.widget.SearchView;
 import android.widget.SearchView.OnQueryTextListener;
 
+import static java.lang.String.CASE_INSENSITIVE_ORDER;
+
 
 public class searchFragment extends Fragment {
 
+    androidx.appcompat.widget.SearchView searchView;
     RecyclerView foodSearchRecycler;
     fooddisplayadapter adapter;
 
@@ -29,6 +32,8 @@ public class searchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        searchView = view.findViewById(R.id.searchfoods);
 
         //recyclerview
         foodSearchRecycler = view.findViewById(R.id.SearchFoodShowRecycler);
@@ -45,7 +50,33 @@ public class searchFragment extends Fragment {
         //end
 
 
+        searchView.setOnQueryTextListener(new androidx.appcompat.widget.SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                processsearch(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                processsearch(query);
+                return false;
+            }
+        });
+
+
         return view;
+    }
+
+    private void processsearch(String query) {
+        FirebaseRecyclerOptions<FoodModel> options =
+                new FirebaseRecyclerOptions.Builder<FoodModel>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Food").orderByChild("name").startAt(query.toLowerCase().toUpperCase()).endAt(query + "\uf8ff"), FoodModel.class)
+                        .build();
+
+        adapter = new fooddisplayadapter(options);
+        adapter.startListening();
+        foodSearchRecycler.setAdapter(adapter);
     }
 
     @Override
